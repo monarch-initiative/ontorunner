88e17788a6703e4e0473808f146ce045ec3b6bd9
Deepak <deepakunni3@users.noreply.github.com>
2021-02-17 17:30:34 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+NAME = 'runner'
+URL = 'https://github.com/monarch-initiative/runner'
+AUTHOR = 'Deepak Unni'
+EMAIL = 'deepak.unni3@gmail.com'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = '0.0.1'
+LICENSE = 'BSD'
+
+with open("requirements.txt", "r") as FH:
+    REQUIREMENTS = FH.readlines()
+
+EXTRAS = {}
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    version=VERSION,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    description=DESCRIPTION,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license=LICENSE,
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[r for r in REQUIREMENTS if not r.startswith("#")],
+    extras_require=EXTRAS,
+    include_package_data=True
+)
