710a2f8b9f454c12939d8a5fe507423bc8d6b55e
Harshad <hrshdhgd@users.noreply.github.com>
2021-02-19 11:03:43 -0600

    rearranged files avd created json2tsv function

--- runner/runner.py
+++ runner/runner.py
@@ -4,10 +4,36 @@ from oger.ctrl.router import Router, PipelineServer
 from oger.doc import EXPORTERS
 from oger.ctrl.run import run
 import configparser
+from kgx.cli.cli_utils import transform
+import os
 
 @click.group()
 def cli():
     pass
+
+@cli.command('json2tsv')
+@click.option('--input', '-i', type=click.Path(exists=True))#, required=True)
+@click.option('--output', '-o', type=str)#, required=True)
+def json2tsv(input, output):
+    """
+    Converts an JSON file into 'nodes' and 'edges' TSV.
+    """
+    if input:
+        if output == None:
+            output = 'data/output'
+
+        transform(inputs=[input], input_format='obojson', output=output, output_format='tsv')
+    else:
+        input_folder = 'data/input/'
+        output_folder = 'data/output/'
+
+        for subdir, dirs, files in os.walk(input_folder):
+            for file in files:
+                fn, ext = os.path.splitext(file)
+
+                if ext == '.json':
+                    transform(inputs=[subdir+file], input_format='obojson', output=output_folder+fn, output_format='tsv')
+                
 
 @cli.command('prepare-termlist')
 @click.option('--input', '-i', type=click.Path(exists=True), required=True)
