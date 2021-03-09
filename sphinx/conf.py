f91c4ca32ad83bf3a8e78f32f71c4052d69fabed
Harshad <hrshdhgd@users.noreply.github.com>
2021-03-09 11:19:16 -0600

    added autodoc again

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -31,7 +31,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon' ,
+    'sphinx.ext.autodoc',
+   'sphinx.ext.napoleon',
    'sphinx_rtd_theme',
    'recommonmark'
 ]
