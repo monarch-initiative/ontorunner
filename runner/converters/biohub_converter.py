d4d59fbb6bcbfbaca01bfee8af3781e0ed92aeab
Harshad <hrshdhgd@users.noreply.github.com>
2021-03-29 16:44:42 -0500

    bug fix

--- runner/converters/biohub_converter.py
+++ runner/converters/biohub_converter.py
@@ -58,8 +58,9 @@ def parse(input_filename, output_filename) -> None:
                 for s in synonyms.split('|'):
                     syn_record = [x for x in parsed_record]
                     syn_record[3] = s
-                    if syn_record[2] != s:
+                    if s != syn_record[4]:
                         syn_record[2] = syn_record[2]+'_SYNONYM'
+                    import pdb; pdb.set_trace()
                     write_line(syn_record, OUTSTREAM)
             write_line(parsed_record, OUTSTREAM)
 
