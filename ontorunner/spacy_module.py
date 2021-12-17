c8c4bdbf15e4f53d2f3428351fc377ccd1d6f8ee
Harshad Hegde <hegdehb@gmail.com>
2021-12-16 23:12:54 -0600

    better output but still slow

--- ontorunner/spacy_module.py
+++ ontorunner/spacy_module.py
@@ -16,7 +16,7 @@ from multiprocessing import freeze_support
 # python -m spacy download en_core_web_sm
 # nlp = spacy.load("en_core_web_sm")
 nlp = spacy.load("en_ner_craft_md")
-onto = OntoExtractor.OntoExtractor(nlp)
+# onto = OntoExtractor.OntoExtractor(nlp)
 
 output_columns = [
     "document_id",
@@ -80,37 +80,64 @@ def onto_extractor(doc):
 
 def get_knowledgeBase_enitities(doc):
     linker = nlp.get_pipe("scispacy_linker")
-    ent_list = []
+    ent_dict = {}
+    key_list = ["cui", "name", "aliases", "definition", "tui"]
+    for k in key_list:
+        ent_dict[k] = []
+    df = pd.DataFrame()
 
     for entity in doc.ents:
         for kb_ent in entity._.kb_ents:
-            ent_dict = {}
             ent_object = linker.kb.cui_to_entity[kb_ent[0]]
-            ent_dict["cui"] = ent_object.concept_id
-            ent_dict["name"] = ent_object.canonical_name
-            ent_dict["aliases"] = ent_object.aliases
-            ent_dict["definition"] = ent_object.definition
-            ent_dict["tui"] = ent_object.types
-            ent_list.append(ent_dict)
-    return ent_list
+            ent_dict["cui"].append(ent_object.concept_id)
+            ent_dict["name"].append(ent_object.canonical_name)
+            ent_dict["aliases"].append(ent_object.aliases)
+            ent_dict["definition"].append(ent_object.definition)
+            ent_dict["tui"].append(ent_object.types)
+
+    return pd.DataFrame.from_dict(ent_dict)
 
 
 def get_token_info(doc):
+    key_list = [
+        "name",
+        "POS",
+        "tag",
+        "curie",
+        "category",
+        "synonym_of",
+        "source",
+        "sentence",
+        "start",
+        "end",
+    ]
     onto_dict = {}
+    for k in key_list:
+        onto_dict[k] = []
+    df = pd.DataFrame()
     for token in doc:
         if token._.is_an_onto_term:
-            onto_dict["name"] = token.text
-            onto_dict["POS"] = token.pos_
-            onto_dict["tag"] = token.tag_
-            onto_dict["curie"] = token._.curie
-            onto_dict["category"] = token._.category
-            onto_dict["synonym_of"] = token._.synonym_of
-            onto_dict["source"] = token._.source
-            onto_dict["sentence"] = token._.sentence
-            onto_dict["start"] = token._.start
-            onto_dict["end"] = token._.end
+            onto_dict["name"].append(token.text)
+            onto_dict["POS"].append(token.pos_)
+            onto_dict["tag"].append(token.tag_)
+            onto_dict["curie"].append(token._.curie)
+            onto_dict["category"].append(token._.category)
+            onto_dict["synonym_of"].append(token._.synonym_of)
+            onto_dict["source"].append(token._.source)
+            onto_dict["sentence"].append(token._.sentence)
+            onto_dict["start"].append(token._.start)
+            onto_dict["end"].append(token._.end)
 
-    return onto_dict
+    return pd.DataFrame.from_dict(onto_dict)
+
+
+def explode_df(df: pd.DataFrame):
+    new_df = pd.DataFrame()
+    for _, doc_obj_row in df.iterrows():
+        tmp_df = doc_obj_row.spacy_kb_ent
+        tmp_df.insert(0, "id", doc_obj_row.id)
+        new_df = pd.concat([new_df, tmp_df], ignore_index=True)
+    return new_df
 
 
 def doc_to_df(dframcy: DframCy, df: pd.DataFrame) -> pd.DataFrame:
@@ -121,23 +148,17 @@ def doc_to_df(dframcy: DframCy, df: pd.DataFrame) -> pd.DataFrame:
     #     dframcy_df = pd.concat([dframcy_df, tmp_df])
     # return dframcy_df
     df_of_df = pd.DataFrame()
-    dframcy_df = pd.DataFrame()
     df_of_df["id"] = df["id"]
     df_of_df["spacy_doc"] = (
         df["spacy_doc"].apply(lambda row: dframcy.to_dataframe(row)).to_frame()
     )
 
-    for _, doc_obj_row in df_of_df.iterrows():
-        tmp_df = doc_obj_row.spacy_doc
-        tmp_df.insert(0, "id", doc_obj_row.id)
-        dframcy_df = pd.concat([dframcy_df, tmp_df], ignore_index=True)
-
-    return dframcy_df
+    return explode_df(df_of_df)
 
 
 def export_tsv(df: pd.DataFrame, fn: str) -> None:
-    path = os.path.join(PARENT_DIR, "data/output/" + fn + ".tsv")
-    df.to_csv(fn, sep="\t", index=None)
+    fn_path = os.path.join(PARENT_DIR, "data/output/" + fn + ".tsv")
+    df.to_csv(fn_path, sep="\t", index=None)
 
 
 def main():
@@ -161,8 +182,8 @@ def main():
         lambda row: get_token_info(row)
     )
 
-    kb_df = input_df[["id", "spacy_kb_ent"]]
-    onto_df = input_df[["id", "spacy_tokens"]]
+    kb_df = explode_df(input_df[["id", "spacy_kb_ent"]])
+    onto_df = explode_df(input_df[["id", "spacy_tokens"]])
     nlp_df = doc_to_df(dframcy, input_df[["id", "spacy_doc"]])
 
     export_tsv(kb_df, "kb_entities_output")
@@ -171,4 +192,6 @@ def main():
 
 
 if __name__ == "__main__":
+    freeze_support()
+    onto = OntoExtractor.OntoExtractor(nlp)
     main()
