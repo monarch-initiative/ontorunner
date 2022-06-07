45a7b91d85b1f12ba68c21a23ce29767fbb55c23
Harshad Hegde <hegdehb@gmail.com>
2022-06-07 10:52:19 -0500

    unify version source

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from ..setup import VERSION
 
 sys.path.insert(0, os.path.abspath("../.."))
 
@@ -23,7 +24,7 @@ copyright = "2021, Harshad Hegde"
 author = "Harshad Hegde"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = VERSION
 
 
 # -- General configuration ---------------------------------------------------
