6e03d22962b09206a16c94981bd125c0b164e0bd
Harshad Hegde <hegdehb@gmail.com>
2022-06-16 12:43:13 -0500

    implemented BERO

--- ontorunner/pipes/OntoRuler.py
+++ ontorunner/pipes/OntoRuler.py
@@ -202,9 +202,10 @@ class OntoRuler(object):
                 "origin": origin,
             }
             pattern_dict["id"] = object_id
-            pattern_dict["label"] = origin.split(".")[
-                0
-            ]  # could be object_id/object_label
+            # pattern_dict["label"] = origin.split(".")[
+            #     0
+            # ]  # could be object_id/object_label
+            pattern_dict["label"] = object_id.replace("_SYNONYM", "")
             pattern_dict["pattern"] = matched_term
 
         return terms_dict, pattern_dict, self.nlp(matched_term)
